1952e9bf50da0569b0dafacf0c127f8a4ec03e22
fabianomg <fabianodasilva496@gmail.com>
2020-03-26 20:16:46 -0300

    teste

--- controllers/ValidCardController.js
+++ controllers/ValidCardController.js
@@ -10,16 +10,16 @@ const Cache = Redis.createClient(6379, "redis");
 module.exports = {
 
     async Card(req, res) {
-// aciona afila dos token
-        Queue.consume(true, '#'+req.body.id + 'token', (message) => {
+        // aciona afila dos token
+        Queue.consume(true, '#' + req.body.id + 'token', (message) => {
             if (message) {
                 Amarithcafe.cardValidation(req.body.id, req.body.username, message.content.toString())
             }
         })
-        
-              Queue.sendToQueue(false, '#' + req.body.id+ 'atividade', ' Resolvendo captcha......')
-              Queue.sendToQueue(false, '#' + req.body.id+ 'verificarcards', ' ok')
-        
+
+        Queue.sendToQueue(false, '#' + req.body.id + 'atividade', ' Resolvendo captcha......')
+        Queue.sendToQueue(false, '#' + req.body.id + 'verificarcards', ' ok')
+
     },
     async save(req, res) {
 
@@ -40,42 +40,53 @@ module.exports = {
                     }
                 });
 
-               CadastroCards.find({'card':{'number':cardSeparetion[0]}},(arr,result)=>{
-             console.log(result)
-               if(result ==''){
-                
-                 Queue.sendToQueue(false, '#' + c.userID + 'carregados', 1)
-              cad.save((error) => {
-              if (error) {
-                        console.log(error.message)
-                        let e = error.message.indexOf('to be unique');
-                        let ee = error.message.indexOf('dup key');
-                        if (e != -1 || ee  !=-1) {
-                            Cache.set('duplicados', 0, () => {
-                                Cache.incr('duplicados');
-                            });
-                            Cache.expire('duplicados', 30);
-                            Queue.sendToQueue(false, '#' + c.userID + 'atividade', c.cards[i] + ' já existe na nossa base de dados..')
-                            let l = new Logs({ arq: 'ValidationController', type: 'card', msg: error.message })
-                            l.save();
-                        }
+                CadastroCards.find({ 'card.number': cardSeparetion[0] }, async (arr, result) => {
+                    let card;
+                    (result != '') ? card = result[0].card.number + '|' + result[0].card.mes + '|' + result[0].card.ano + '|' + result[0].card.cvv : card = '';
+                    if (result == '') {
+                        Queue.sendToQueue(false, '#' + c.userID + 'carregados', 1)
 
-                    } else {
-                        Cache.set('cadastrados', 0, () => {
-                            Cache.incr('cadastrados');
+                        cad.save((error) => {
+                            if (error) {
+                                console.log(error.message)
+                                let e = error.message.indexOf('to be unique');
+                                let ee = error.message.indexOf('dup key');
+                                if (e != -1 || ee != -1) {
+                                    Cache.set('duplicados', 0, () => {
+                                        Cache.incr('duplicados');
+                                    });
+                                    Cache.expire('duplicados', 30);
+                                    Queue.sendToQueue(false, '#' + c.userID + 'atividade', c.cards[i] + ' já existe na nossa base de dados..')
+                                    let l = new Logs({ arq: 'ValidationController', type: 'card', msg: error.message })
+                                    l.save();
+                                }
+
+                            } else {
+                                Queue.sendToQueue(false, '#' + c.userID + 'atividade', c.cards[i] + ' Cadastrado com sucesso ...')
+                            }
                         });
-                        Cache.expire('cadastrados', 30);
-                        Queue.sendToQueue(false, '#' + c.userID + 'atividade', c.cards[i] + ' Cadastrado com sucesso ...')
+                    } else {
+
+                        let valid = result[0].valid
+                        if (valid) {
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'atividade', 'Teste finalizado')
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'reprovados', 1)
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'testados', 1)
+                            let MSG = '<a href="#">' + card + '  </a><span class="label label-sm label-danger">NEGADO</span> <span class="label label-sm label-success">TESTADO</span>'
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'listreprovados', MSG)
+                        } else {
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'atividade', 'Teste finalizado')
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'reprovados', 1)
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'testados', 1)
+                            let MSG2 = '<a href="#">' + card + '  </a><span class="label label-sm label-danger">NEGADO</span> <span class="label label-sm label-info">ELPATRON</span>'
+                            Queue.sendToQueue(false, '#' + req.body.userID + 'listreprovados', MSG2)
+                        }
                     }
                 });
-          }else{
-            Queue.sendToQueue(false, '#' + c.userID + 'duplicados', 1)
-          }
-                });
-          }
+            }
 
-         
-           Queue.sendToQueue(false, '#' + c.userID + 'atividade', ' Aguarde.......')
+
+            Queue.sendToQueue(false, '#' + c.userID + 'atividade', ' Aguarde.......')
 
             if (cadastrados != 0 && duplicados != 0) {
 
@@ -87,12 +98,17 @@ module.exports = {
             if (cadastrados = 0 && duplicados != 0) {
                 Queue.sendToQueue(false, '#' + c.userID + 'atividade', ' todos os ' + cadastrados + ' que você inseriu já exite na nossa base de dados')
             }
-
-
+            const cardstestados = await CadastroCards.where({ tested: false, owner: username, userID: id }).limit(1);
+            if (cardstestados == '') {
+                Queue.sendToQueue(false, '#' + c.userID + 'status', 'Aguardando...')
+                Queue.sendToQueue(false, '#' + c.userID + 'atividade', 'Processo finalizado com sucesso!!')
+            } else {
+                Queue.sendToQueue(false, '#' + c.userID + 'verificarcards', 'ok')
+            }
         } catch (error) {
             let l = new Logs({ arq: 'ValidationController', type: 'errorcatch', msg: error.message })
             l.save();
-            Queue.sendToQueue(false, c.userID + '#cadastro#cards', 'false')
+            Queue.sendToQueue(false, '#' + c.userID + 'cadastrocards', 'false')
         }
     },
     async teste(req, res) {
@@ -166,7 +182,7 @@ module.exports = {
     },
     async getcards(req, res) {
         console.log(req.query.id)
-        const query = await CadastroCards.where({ userID: req.query.id,valid:true });
+        const query = await CadastroCards.where({ userID: req.query.id, valid: true });
 
         let result = []
 
